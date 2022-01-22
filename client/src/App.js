5c3c108da744a9ada8b516971bf314741e293deb
Mikhaillkin <ronaldovah@mail.ru>
2022-01-22 22:12:54 +0300

    Initial commit,primamry template web app

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.scss';
+import 'materialize-css/sass/materialize.scss';
+
+function App() {
+  return (
+      <div className="container">
+        <h1>HELLO</h1>
+      </div>
+  );
+}
+
+export default App;
